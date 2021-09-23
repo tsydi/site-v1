1d92cb0081fe36ce044d09b0525c94d15e23e102
TsydykhP <tsydykhp@gmail.com>
2021-09-24 04:39:49 +0800

    changed on white icons color

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -3,16 +3,16 @@ import { ExternalLink } from "./NavToolKit";
 
 export const SocialIcons = () => {
   return (
-    <div className="my-6 md:my-0 iconsColor text-2xl md:text-xl flex flex items-center justify-center">
-      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="bg-gray-100 p-1 rounded-full md:flex items-center justify-center">
+    <div className="my-6 md:my-0 text-white text-2xl md:text-xl flex flex items-center justify-center">
+      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="p-1 rounded-full md:flex items-center justify-center">
         <AiFillGithub />
       </ExternalLink>
 
-      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="bg-gray-100 p-1 mx-4 rounded-full md:flex items-center justify-center">
+      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="p-1 mx-4 rounded-full md:flex items-center justify-center">
         <AiFillLinkedin />
       </ExternalLink>
 
-      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="bg-gray-100 p-1 rounded-full md:flex items-center justify-center">
+      <ExternalLink href="https://github.com/tsydykh-pokhodiev" className="p-1 rounded-full md:flex items-center justify-center">
         <AiFillTwitterCircle />
       </ExternalLink>
     </div>
