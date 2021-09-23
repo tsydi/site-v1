7a18112dfb1ec0a175dcba505fde7446d2dbe53b
TsydykhP <tsydykhp@gmail.com>
2021-09-24 04:44:00 +0800

    text displayed better on phones screens (font-size is changed)

--- components/NavAfter.tsx
+++ components/NavAfter.tsx
@@ -9,7 +9,7 @@ export const NavAfter = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 2.75 }}
     >
-      <span className="text-white">
+      <span className="text-white text-xs md:text-base">
         I am open for a remote job! * Check
         <ExternalLink
           href="https://drive.google.com/file/d/1SLYPurTDccShrs43lrU8GDeelNG1OEEx/view?usp=sharing"
